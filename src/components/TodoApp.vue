ebec29b34c35fcfc21d5bf34d9b71945e8745ee6
nanamihirooka <nanamihirooka@gmail.com>
2024-01-08 17:01:04 +0900

    Add Todo List

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Vue Todo App</h1>
+  <div v-for="todo in todos" :key="todo.id">
+    {{ todo.text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  data () {
+    return {
+      todos: [
+        { id: 1, text: 'task1' },
+        { id: 2, text: 'task2' },
+        { id: 3, text: 'task3' },
+      ]
+    }
+  }
+}
+</script>
